a4ebb599ed8a48bbe1870ab3c9a14ceb98129c5b
CamnKim <ckim18@kent.edu>
2021-03-11 18:51:59 -0500

    fixed gh pages routing issue

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,18 @@
 import React from "react"
 import { Navbar, Nav } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 function Navigation() {
     return (
         <Navbar sticky="top" collapseOnSelect expand="md" variant="dark">
-            <Navbar.Brand href="/">COVID STATS</Navbar.Brand>
+            <Navbar.Brand as={link} to="/">COVID STATS</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto ml-2">
-                    <Nav.Link href="/US" className="ml-3">
+                    <Nav.Link as={link} to="/US" className="ml-3">
                         US
                     </Nav.Link>
-                    <Nav.Link href="/About" className="ml-3">
+                    <Nav.Link as={link} to="/About" className="ml-3">
                         About
                     </Nav.Link>
                 </Nav>
