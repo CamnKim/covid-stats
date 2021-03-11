eaa134c3fd06a6c7c99af22db23fc8c38d72335a
CamnKim <ckim18@kent.edu>
2021-03-11 18:53:53 -0500

    fixed spelling error for Link in Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom"
 function Navigation() {
     return (
         <Navbar sticky="top" collapseOnSelect expand="md" variant="dark">
-            <Navbar.Brand as={link} to="/">COVID STATS</Navbar.Brand>
+            <Navbar.Brand as={Link} to="/">COVID STATS</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto ml-2">
-                    <Nav.Link as={link} to="/US" className="ml-3">
+                    <Nav.Link as={Link} to="/US" className="ml-3">
                         US
                     </Nav.Link>
-                    <Nav.Link as={link} to="/About" className="ml-3">
+                    <Nav.Link as={Link} to="/About" className="ml-3">
                         About
                     </Nav.Link>
                 </Nav>
